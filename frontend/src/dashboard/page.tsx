5c93d62f2300d592d7ea9b8d4ed5434780c504bb
nicodemus-munywoki <79452253+nicodemus-munywoki@users.noreply.github.com>
2025-12-16 16:17:31 +0000

    changes

---
+++ frontend/src/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='p-4'>
+      <div>
+        1
+      </div>
+      <div>
+        2
+      </div>
+    </div>
+  )
+}
+
+export default page
